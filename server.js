23f862db9f96c799a33717908c913a8553092767
Abzy2020 <90886567+Abzy2020@users.noreply.github.com>
2022-05-25 17:14:46 -0400

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const cheerio = require("cheerio");
 const axios = require("axios");
 
-const PORT = PORT;
+const PORT = <PORT>;
 const app = express();
 const headlines = [];
 const uniqueHeadlines = [];
